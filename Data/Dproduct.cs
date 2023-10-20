a87b66190c02d9dae7187f855894374b983296cd
Jimp172 <jim.mayhuire@tecsup.edu.pe>
2023-10-20 15:33:49 -0500

    Agregar archivos de proyecto.

---
+++ Data/Dproduct.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace Data
+{
+    public class Dproduct
+    {
+        private string connectionString = "Data Source=DESKTOP-GRUDL8S;Initial Catalog=FacturaDB;User ID=tecsup;Password=tecsup";
+
+        public List<Product> Get()
+        {
+            List<Product> productList = new List<Product>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open(); // Abre la conexión
+
+                using (SqlCommand command = new SqlCommand("ListarProducto", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Product product = new Product
+                            {
+                                product_id = reader.GetInt32(reader.GetOrdinal("product_id")),
+                                Name = reader.GetString(reader.GetOrdinal("Name")),
+                                price = reader.GetDecimal(reader.GetOrdinal("price")),
+                                stock = reader.GetInt32(reader.GetOrdinal("stock")),
+                                active = reader.GetInt32(reader.GetOrdinal("active")) == 1 ? true : false
+                            };
+
+                            productList.Add(product);
+                        }
+                    }
+                }
+            }
+            return productList;
+        }
+    }
+}
